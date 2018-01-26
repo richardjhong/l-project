dbe242a5013b2d3df591bc35da8cde5b1812d34d
git push -u origin <rhong24@gmail.com>
2018-01-25 20:50:46 -0800

    structuring app.post

---
+++ README.md
@@ -0,0 +1,1 @@
+Written by Richard Hong, takes arguments x and y in req.body and outputs sum in JSON
---
+++ app.js
@@ -0,0 +1,17 @@
+var express = require('express');
+var app = express();
+
+var bodyParser = require('body-parser');
+app.use(bodyParser.json());
+
+app.post('/test', function(req, res) {
+  let x = parseInt(req.body.x) || null
+  let y = parseInt(req.body.y) || null
+  // res.setHeader('Content-Type', 'application/json');
+
+  res.send(JSON.stringify({"sum": x+y}))
+})
+
+app.listen(3000, function() {
+  console.log('Server is running on port 3000')
+});
